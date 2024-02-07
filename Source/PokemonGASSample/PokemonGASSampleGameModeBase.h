661afabe562744af309edc0934845a72f87b648c
JeonMinSu <session_min@naver.com>
2024-02-07 18:11:26 +0900

    [전민수] 프로젝트 초기 세팅

---
+++ Source/PokemonGASSample/PokemonGASSampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PokemonGASSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POKEMONGASSAMPLE_API APokemonGASSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
